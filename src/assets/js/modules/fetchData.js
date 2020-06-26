ed86763c071f066b4d12173997fa50bf178ff4e1
rahul_dahal <rdaahal@gmail.com>
2020-06-26 08:06:19 +0545

    CORS problem resolved

--- src/assets/js/modules/fetchData.js
+++ src/assets/js/modules/fetchData.js
@@ -37,7 +37,7 @@ export function fetchData(dataToBeFetched, id) {
             {
                 headers: {
                     "X-Auth-Token": Key,
-                    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-HeadersOrigin, X-Requested-With, Content-Type, Accept, Authorization",
+                    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Access-Control-Allow-Origin",
                     "Access-Control-Allow-Methods": "PUT, GET, POST, OPTIONS",
                     "Access-Control-Allow-Origin": "*"
 
