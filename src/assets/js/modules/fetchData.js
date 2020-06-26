21c7ce4104357ef41aec8f0ffcb01b68ebedbce8
rahul_dahal <rdaahal@gmail.com>
2020-06-26 09:06:52 +0545

    CORS and environment variable setup for netlify

--- src/assets/js/modules/fetchData.js
+++ src/assets/js/modules/fetchData.js
@@ -39,6 +39,8 @@ export function fetchData(dataToBeFetched, id) {
                 headers: {
                     "X-Auth-Token": process.env.API_KEY,
 
+                    "Access-Control-Allow-Credentials": "true",
+
                     "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Access-Control-Allow-Origin",
 
                     "Access-Control-Request-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Access-Control-Allow-Origin",
