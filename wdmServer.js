0eac330837061c4cafea7d9532959c9b9e30cf48
Rahul Dahal <rdaahal@gmail.com>
2020-10-29 17:05:07 +0545

    feat: webpack-dev-middleware custom setup

---
+++ wdmServer.js
@@ -0,0 +1,15 @@
+// webpack-dev-middleware setup(bundle in memory)
+
+const app = require("express")();
+const webpack = require("webpack");
+const webpackConfigFile = require("./webpack.config");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const compiler = webpack(webpackConfigFile);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: webpackConfigFile.output.publicPath,
+  })
+);
+
+app.listen(5000, () => console.log("server is running"));
